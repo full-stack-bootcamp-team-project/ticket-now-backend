79cd0b5ef0d6712a855ef673d22e199b588dd5e8
박형빈 <gudqls0991@naver.com>
2025-11-04 17:13:24 +0900

    chore/reservation/controller-update

--- src/main/java/com/ticketnow/reservation/controller/ReservationController.java
+++ src/main/java/com/ticketnow/reservation/controller/ReservationController.java
@@ -42,7 +42,7 @@ public class ReservationController {
         reservationService.deleteReservation(performanceScheduleId, userId, seatId, seatNumber);
     }
 
-    // http://localhost:8080/api/reservation/seat/performanceScheduleId?PS001
+    // http://localhost:8080/api/reservation/seat?performanceScheduleId?PS001
     // 좌석 예약 상태
     @GetMapping("/seat")
     public List<Reservation> getSeatStatus(@RequestParam String performanceScheduleId) {
