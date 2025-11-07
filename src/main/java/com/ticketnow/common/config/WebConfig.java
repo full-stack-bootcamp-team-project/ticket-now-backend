b4d6c82361a2d76e685faf385783b9011858ce05
YOU KI TAE <69350870+tiradovi@users.noreply.github.com>
2025-11-07 16:19:55 +0900

    chore/user-login-util-update

--- src/main/java/com/ticketnow/common/config/WebConfig.java
+++ src/main/java/com/ticketnow/common/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.ticketnow.common.config;
 
 import com.ticketnow.common.interceptor.LoginInterceptor;
+import com.ticketnow.common.interceptor.AlreadyLoginInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -11,30 +12,34 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     private final LoginInterceptor loginInterceptor;
+    private final AlreadyLoginInterceptor alreadyLoginInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        // 일반 유저가 로그인 했을 때 접속할 수 있는 api 리스트
-        registry.addInterceptor(loginInterceptor)
-                .addPathPatterns( // 로그인 유저가 특별히 더 들어갈 수있는 api들
-                        "/reservation",       // 예매 페이지
-                        "/user/myPage"        // 마이 페이지
 
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns(
+                        "/reservation",
+                        "/user/myPage"
                 )
-                .excludePathPatterns( // 누구든지 들어갈 수 있는 api 주소들
-                        "/",                    // 메인 페이지
-                        "/performance",         // 카테고리별 공연 페이지
-                        "/performance/detail",  // 공연 상세 페이지
-                        "/user/login",          // 로그인 페이지
-                        "/user/signup",         // 회원가입 페이지
-                        "/performance/search",  // 공연 검색 페이지
-                        "/user/findId",         // 아이디 찾기 페이지
-                        "/user/findPassword",   // 비밀번호 찾기 페이지
-                        "/css/**",              // css 파일
-                        "/js/**",               // JS 파일
-                        "/images/**",           // Image 파일
-                        "/api/**",              // API 요청
-                        "/user/add"             // 유저 등록
+                .excludePathPatterns(
+                        "/",
+                        "/performance",
+                        "/performance/detail",
+                        "/user/login",
+                        "/user/signup",
+                        "/performance/search",
+                        "/user/findId",
+                        "/user/findPassword",
+                        "/css/**",
+                        "/js/**",
+                        "/images/**",
+                        "/api/**",
+                        "/user/add"
                 );
+
+
+        registry.addInterceptor(alreadyLoginInterceptor)
+                .addPathPatterns("/user/login", "/user/signup");
     }
 }
